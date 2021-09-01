f3fe3b9b4485cf90fc4d2f28ca769d5c4e0fa9f0
Maksim <maksim.gugaev@x5.ru>
2021-09-01 21:22:21 +0300

    Add icteric raiting print

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import aiohttp
 import asyncio
+import adapters
+import pymorphy2
+from text_tools import split_by_words, calculate_jaundice_rate
 
+
+ICTERIC_WORDS = (
+    'опасность',
+    'мощный',
+    'серьёзный',
+)
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -10,8 +19,13 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
-        print(html)
+        html = await fetch(session, 'https://inosmi.ru/military/20210901/250424381.html')
+        plaintext = adapters.SANITIZERS['inosmi_ru'](html, plaintext=True)
+        morph = pymorphy2.MorphAnalyzer()
+        words = split_by_words(morph, plaintext)
+        raiting = calculate_jaundice_rate(words, ICTERIC_WORDS)
+        print(f'Рейтинг: {raiting}')
+        print(f'Слов в статье: {len(words)}')
 
 
 asyncio.run(main())
